6aca7c4938797ff0e3ba868784408fd74dc2f577
Шатура Евгений <shatura.evgy@gmail.com>
2023-05-23 22:03:48 +0200

    lab10

---
+++ test.py
@@ -0,0 +1,9 @@
+def gcd_extended(num1, num2):
+    if num1 == 0:
+        return num2, 0, 1
+    else:
+        div, x, y = gcd_extended(num2 % num1, num1)
+    return div, y - (num2 // num1) * x, x
+
+
+print(gcd_extended(47111, 56973))
